125b3c180717ef6a8875169fde0dc9cf73a7d792
DolyNainggolan <dolynainggolan13@gmail.com>
2019-12-06 16:31:06 +0700

    latihan variabel

---
+++ latihan/Test4.java
@@ -0,0 +1,14 @@
+public class Test4
+{
+
+    public static void main(String args[])
+        {
+            int x = 10;
+
+            if ( x < 20) 
+            {
+                System.out.print("This is if statement");
+            }
+
+        }
+}
---
+++ latihan/Test5.java
@@ -0,0 +1,16 @@
+public class Test5
+{
+    public static void main (String args[])
+    {
+        int x = 30;
+
+        if ( x < 20 )
+        {
+            System.out.print ("This is if Statement");
+        }
+        else
+        {
+            System.out.print ("This is else statement");
+        }
+    }
+}
