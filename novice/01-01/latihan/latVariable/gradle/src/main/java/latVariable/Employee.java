3faa23b59d39985adb85b658a7e8d8c6009c07ae
DolyNainggolan <dolynainggolan13@gmail.com>
2019-12-11 11:05:03 +0700

    latihan variable

---
+++ novice/01-01/latihan/latVariable/gradle/src/main/java/latVariable/Employee.java
@@ -0,0 +1,33 @@
+package latVariable;
+
+public class Employee
+{
+    //this instance variable is visible for any child class
+    public String name;
+
+    //salary variable is visible in Employee class only 
+    private double salary;
+
+    //the name variable is assigned in the constructor
+    public Employee (String empName)
+    {
+        name =empName;
+    }
+    //the salary variable is assigned a value
+    public void setSalary (double empSal)
+    {
+        salary = empSal;
+    }
+    //this method prints the employee details
+    public void printEmp()
+    {
+        System.out.println ("name : " + name );
+        System.out.println ("salary : " + salary);
+    }
+    public static void main (String args[])
+    {
+        Employee empOne = new Employee ("Ransika");
+        empOne.setSalary (1000);
+        empOne.printEmp();
+    }
+}
